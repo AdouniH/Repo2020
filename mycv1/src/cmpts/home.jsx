cde16617eb4fd1c7631e8d0022e5d1106770f461
adouniH <Houssemadouni11@gmail.com>
2020-02-17 21:44:41 +0100

    save changes

--- mycv1/src/cmpts/home.jsx
+++ mycv1/src/cmpts/home.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavigationBar from './navbar.jsx' ;
+import './css/home.css';
 
 function Home() {
 
+  const [code, setCode] = useState("");
+
+  function codeChanged(event){
+      setCode(event.target.value);
+  }
+
+  function submittion(event){
+      event.preventDefault();
+      alert(code)
+  }
+
   return (
+    <body>
       <div>
-        <NavigationBar page='home'/>
-        home
+          <NavigationBar page='home'/>
+          <div class='center'>
+          <div class="container">
+              <form onSubmit={submittion}>
+              <div class="form-group">
+
+                <div class="b">
+                    <label for="usr">Veuillez entrer votre code</label>
+                </div>
+                <div class="b">
+                    <input onChange={codeChanged} type="text" class="form-control" id="code"></input>
+                </div>
+                <div class="b">
+                    <button type="submit" class="btn btn-success bouton">Valider</button>
+                </div>
+
+              </div>
+              </form>
+              </div>
+          </div>
       </div>
+    </body>
   );
 }
 
